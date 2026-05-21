2711dab213ef001de4c46f89e106e9d98a4a3722
Joshua Plumb <joshuamichael1031@gmail.com>
2026-05-21 16:45:02 -0700

    Connection and Program.cs
    Configured the docker postgress database, although there are some package version issues at runtime. Connection string was added, and program.cs was fullt configured.

--- src/HealingMindset.API/Program.cs
+++ src/HealingMindset.API/Program.cs
@@ -2,8 +2,6 @@ using HealingMindset.Repositories.Interfaces;
 using HealingMindset.Repositories.Repositories;
 using HealingMindset.Repositories.Context;
 using Microsoft.EntityFrameworkCore;
-using HealingMindset.Repositories.Shared;
-using Microsoft.AspNetCore.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddScoped<IVideoResourceService, VideoResourceRepository>();
-builder.AddDbContext<VideoResourceDatabaseContext>(options => options.)
+builder.Services.AddDbContext<VideoResourceDatabaseContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ src/HealingMindset.Repositories/Migrations/20260521220848_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HealingMindset.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoResources",
+                columns: table => new
+                {
+                    VideoId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    YoutubeId = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "character varying(4000)", maxLength: 4000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoResources", x => x.VideoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoResources");
+        }
+    }
+}
