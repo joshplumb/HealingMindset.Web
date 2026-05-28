7e307b89b113772d518d6304ef2d1995c85a049d
Joshua Plumb <joshuamichael1031@gmail.com>
2026-05-28 10:42:02 -0700

    Removed Lambda Endpoints
    I replaced the lambda endpoints with their own explicit methods that will allow me to cleanly implement request filtering.

--- src/HealingMindset.API/Endpoints/VideoResourceEndpoints.cs
+++ src/HealingMindset.API/Endpoints/VideoResourceEndpoints.cs
@@ -9,16 +9,36 @@ namespace HealingMindset.Api.Endpoints
         {
             var videoResourceGroup = app.MapGroup("/api/videos");
 
-            videoResourceGroup.MapPost("/", async (VideoResourceModel videoResource, IVideoResourceService videoService) =>
-                await videoService.Create(videoResource));
-            videoResourceGroup.MapGet("/", async (IVideoResourceService videoService) =>
-                await videoService.GetAll());
-            videoResourceGroup.MapGet("/{videoResourceId:int}", async(int videoResourceId, IVideoResourceService videoService) =>
-                await videoService.GetByID(videoResourceId));
-            videoResourceGroup.MapPut("/", async(VideoResourceModel videoResource, IVideoResourceService videoService) =>
-                await videoService.Update(videoResource));
-            videoResourceGroup.MapDelete("/{videoResourceId:int}", async (int videoResourceId, IVideoResourceService videoService) =>
-                await videoService.Delete(videoResourceId));
+            videoResourceGroup.MapPost("/", CreateVideo);
+            videoResourceGroup.MapGet("/", GetAllVideos);
+            videoResourceGroup.MapGet("/{videoResourceId:int}", GetVideoByID);
+            videoResourceGroup.MapPut("/", UpdateVideo);
+            videoResourceGroup.MapDelete("/{videoResourceId:int}", DeleteVideo);
+        }
+        public static async Task<IResult> CreateVideo(VideoResourceModel videoResource,IVideoResourceService videoService)
+        {
+            await videoService.Create(videoResource);
+            return TypedResults.Created($"/api/videos", videoResource);
+        }
+        public static async Task<IResult> GetAllVideos(IVideoResourceService videoService)
+        {
+            var videos = await videoService.GetAll();
+            return TypedResults.Ok(videos);
+        }
+        public static async Task<IResult> GetVideoByID(int videoResourceId, IVideoResourceService videoService)
+        {
+            var video = await videoService.GetByID(videoResourceId);
+            return video is null ? TypedResults.NotFound() : TypedResults.Ok(video);
+        }
+        public static async Task<IResult> UpdateVideo(VideoResourceModel videoResource, IVideoResourceService videoService)
+        {
+            var video = await videoService.Update(videoResource);
+            return TypedResults.NoContent();
+        }
+        public static async Task<IResult> DeleteVideo(int videoResourceId, IVideoResourceService videoService)
+        {
+            await videoService.Delete(videoResourceId);
+            return TypedResults.NoContent();
         }
     }
 }
